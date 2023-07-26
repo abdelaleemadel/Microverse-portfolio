2facbec15932f884138bfa9d1a0d69d67df5d65d
AbdelaleemAhmed <ahmedadel2912@gmail.com>
2023-07-26 02:50:42 +0200

    Make Nav Bar

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let openMenu = document.querySelector(".fa-bars");
+let closeMenu = document.querySelector(".overLay .fa-xmark");
+let nav = document.querySelector("nav>:first-child");
+let overLay = document.querySelector(".overLay");
+
+function openMenuF() {
+    nav.classList.replace("d-block", "d-none");
+    overLay.classList.replace("d-none", "d-block");
+}
+function closeMenuF() {
+    overLay.classList.replace("d-block", "d-none");
+    nav.classList.replace("d-none", "d-block");
+}
+
+openMenu.addEventListener("click", function () { openMenuF() });
+console.log(nav, overLay)
+closeMenu.addEventListener("click", function () { closeMenuF() })
